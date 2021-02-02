1c2d4ed5bb89d792a35c1ea5de25bdc768b8aa3c
kailanmao <78109823+kailanmao@users.noreply.github.com>
2021-02-01 20:36:35 -0800

    Add files via upload

---
+++ unit01/StarsRunner.java
@@ -0,0 +1,23 @@
+//© A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class StarsRunner
+{
+   public static void main(String args[])
+   {
+      //instantiate a StarsAndStripes object
+      StarsAndStripes x = new StarsAndStripes();
+
+      //call the methods needed to make the patterns on the word document
+      x.printABigBox();
+      x.printTwoBlankLines();
+      x.printTwentyDashes();
+   }
+}
