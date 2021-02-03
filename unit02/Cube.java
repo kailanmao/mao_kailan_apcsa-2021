996a8ccc47b7ed40e025bfe8d6fb6b5e9f2d0a15
kailanmao <78109823+kailanmao@users.noreply.github.com>
2021-02-03 00:05:42 -0800

    Add files via upload

---
+++ unit02/Cube.java
@@ -0,0 +1,14 @@
+//(c) A+ Computer Science
+// www.apluscompsci.com
+//Name - Kailan Mao
+//Date - 2/2/21
+
+public class Cube
+{
+	public static double area( int side )
+	{
+		double sa = 6 * side * side;
+		return sa;
+
+	}
+}
---
+++ unit02/Trap.java
@@ -0,0 +1,13 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name - KAILAN M
+//Date - 2/2/21
+//Class - AP CSA
+//Lab  - TRAP
+
+public class Trap
+{
+	public static double area( int base1, int base2, int height )
+	{
+		return (double)(base1 + base2) * height * 0.5;
+	}
+}
