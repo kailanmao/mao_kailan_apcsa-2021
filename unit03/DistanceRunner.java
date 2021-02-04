af206138b69909a3b8ff2b1aabd8275fc37b7f68
kailanmao <78109823+kailanmao@users.noreply.github.com>
2021-02-03 20:19:15 -0800

    Add files via upload

---
+++ unit03/DistanceRunner.java
@@ -0,0 +1,36 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import java.util.Scanner; 
+import static java.lang.System.*;
+import static java.lang.Math.*;
+
+public class DistanceRunner
+{
+	public static void main( String[] args )
+	{
+		//add test cases	
+		Scanner keyboard = new Scanner(in);
+
+		out.print("Enter x1 :: ");
+		int x1 = keyboard.nextInt();
+
+		out.print("Enter y1 :: ");
+		int y1 = keyboard.nextInt();
+
+		out.print("Enter x2 :: ");
+		int x2 = keyboard.nextInt();
+		
+		out.print("Enter y1 :: ");
+		int y2 = keyboard.nextInt();
+		
+		Distance d = new Distance(x1,y1,x2,y2);
+		d.calcDistance();
+		d.print();
+	}
+}
---
+++ unit03/MilesPerHour.java
@@ -0,0 +1,52 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import java.util.Scanner; 
+import static java.lang.System.*;
+import static java.lang.Math.*;
+
+public class MilesPerHour
+{
+	private int distance, hours, minutes;
+	private double mph;
+
+	public MilesPerHour()
+	{
+		setNums(0,0,0);
+		mph=0.0;
+	}
+
+	public MilesPerHour(int dist, int hrs, int mins)
+	{
+		setNums(dist, hrs, mins);
+	}
+
+	public void setNums(int dist, int hrs, int mins)
+	{
+		distance = dist;
+		hours = hrs;
+		minutes = mins;
+	}
+
+	public void calcMPH()
+	{
+		mph = (double) (60 * distance) / (minutes + 60 * hours);
+	}
+
+	public void print()
+	{
+		out.print(mph);
+	}
+	
+	//create a print or toString or both
+	
+	public String toString()
+	{
+		return "+mph+";
+	}
+}
